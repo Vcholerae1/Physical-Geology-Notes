6b2ca508934866958e65bdfee89ee60b58bac974
barkure <43804451+barkure@users.noreply.github.com>
2024-11-18 14:41:47 +0800

    更新考研笔记大纲链接格式，添加mkdocs构建功能以支持自动化部署

--- deploy.py
+++ deploy.py
@@ -1,5 +1,19 @@
-# 部署到服务器
 import subprocess
+import os
+
+
+def build(mkdocs_path):
+    # 保存当前工作目录
+    original_dir = os.getcwd()
+    try:
+        # 切换到指定目录
+        os.chdir(mkdocs_path)
+        # 生成静态网站
+        subprocess.run(['mkdocs', 'build'], check=True)
+    finally:
+        # 切换回原始目录
+        os.chdir(original_dir)
+
 
 def deploy(build_path, server_site_path, server):
     # 删除服务器站点路径下的所有文件
@@ -11,12 +25,17 @@ def deploy(build_path, server_site_path, server):
     # 重载 Caddy 服务器
     subprocess.run(['ssh', server, 'systemctl reload caddy'], check=True)
 
+
 if __name__ == "__main__":
+    # 本地 mkdocs 项目路径
+    mkdocs_path = './Physical-Geology'
     # 本地构建路径
     build_path = './Physical-Geology/site'
     # 服务器站点路径
     server_site_path = '/var/www/geo'
     # 服务器地址
     server = 'clawcloud-hk'
-
+    # 构建
+    build(mkdocs_path)
+    # 部署
     deploy(build_path, server_site_path, server)
